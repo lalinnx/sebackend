e1237f96dfecb2fd48027f063059bb148d814e04
lalinnx <76035493+lalinnx@users.noreply.github.com>
2023-03-06 20:01:58 +0700

    Update parser_.py

--- parser_.py
+++ parser_.py
@@ -1,4 +1,5 @@
 from tokens import TokenType
+from node import Choice,Question,Group
 
 class Parser:
     def __init__(self, tokens) :
@@ -21,73 +22,75 @@ class Parser:
     def parse(self):
         while self.current_token != None:
             if self.current_token.type in (TokenType.CHAR):
-                if self.current_token == "G":
+                if self.current_token.value == "G":
                     result = self.parseGroup()
-                elif self.current_token == "Q": 
+                elif self.current_token.value == "Q": 
                     result = self.parseQuestion()
             else: self.raise_error()
 
         return result
 
     def parseGroup(self):
-        self.checktype("LEFTCURLY")
+        self.checktype(TokenType.LEFTCURLY)
         name = self.parseName()
-        self.current_token.type in("RIGHTCURLY")
-        self.checktype("LEFTSQUARE")
+        self.current_token.type in(TokenType.RIGHTCURLY)
+        self.checktype(TokenType.LEFTSQUARE)
         self.advance
 
         question = []
         i = 0  
 
         while True:
-            self.current_token == "Q"
-            question[i] = self.parseQuestion()
-            i+=1
-            self.advance
-            if self.current_token.type in ("RIGHTAQUARE"): break
-        return question
+            if self.current_token.value == "Q":
+                question[i] = self.parseQuestion()
+                i+=1
+                self.advance
+            else: self.raise_error()
+            if self.current_token.type in (TokenType.RIGHTAQUARE): break
+
+        return Group(name,question)
         
     def parseQuestion(self):
-        self.checktype("LEFTCURLY")
+        self.checktype(TokenType.LEFTCURLY)
         name = self.parseName()
-        self.current_token.type in("COMMA")
-        self.checktype("CHAR")
-        Question = self.current_token
-        self.checktype("QUESTION")
-        self.checktype("RIGHTCURLY")
-        self.checktype("COMMA")
-        self.checktype("CHAR")
-        if self.current_token == "rand":
+        self.current_token.type in(TokenType.COMMA)
+        self.checktype(TokenType.CHAR)
+        ques = self.parseName
+        self.checktype(TokenType.QUESTION)
+        self.checktype(TokenType.RIGHTCURLY)
+        self.checktype(TokenType.COMMA)
+        self.checktype(TokenType.CHAR)
+        if self.current_token.value == "rand":
             random = True
-        elif self.current_token == "norand":
+        elif self.current_token.value == "norand":
             random = False
         else: self.raise_error()
         choice = self.parseChoice()
 
-        return name+Question+random+choice
+        return Question(name,ques,random,choice)
     
     def parseChoice(self):
-        self.checktype("LEFTSQUARE")
+        self.checktype(TokenType.LEFTSQUARE)
         self.advance
         choice = []
         i = 0     
         while True:
-            self.current_token == "C"
-            self.checktype("LEFTCURLY")
+            self.current_token.value == "C"
+            self.checktype(TokenType.LEFTCURLY)
             choice[i] = self.parseName() 
-            self.current_token.type in("RIGHTCURLY")
-            if self.checktype("ANSWER"):
+            self.current_token.type in(TokenType.RIGHTCURLY)
+            if self.checktype(TokenType.ANSWER):
                 ans = choice[i]
             i+=1
             self.advance
-            if self.current_token.type in ("RIGHTAQUARE"): break
+            if self.current_token.type in (TokenType.RIGHTAQUARE): break
 
-        return choice+ans
+        return Choice(choice,ans)
 
     def parseName(self):
         str = self.current_token
         self.advance()
-        while self.current_token.type in ("CHAR" | "NUMBER"):        
+        while self.current_token.type in (TokenType.CHAR | TokenType.NUMBER):        
             str+=self.current_token
             self.advance()
         return str
