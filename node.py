16af79efa946f0ed72b5da95b497d14118ea94e5
lalinnx <76035493+lalinnx@users.noreply.github.com>
2023-03-06 20:00:54 +0700

    Add files via upload

---
+++ node.py
@@ -0,0 +1,22 @@
+class Choice(object):
+    choice: list
+    Ans: any
+
+    def __repr__(self):
+        return self.choice+self.Ans
+
+class Question():
+    name: any
+    ques: any
+    ran: bool
+    choice: Choice
+
+    def __repr__(self):
+        return self.name+self.ques+self.ran+self.choice
+
+class Group():
+    name: any
+    Question: list
+
+    def __repr__(self):
+        return self.name+self.Question
